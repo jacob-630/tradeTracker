34a16a6ed8e5d15fc1b80229dd4acb979b09a4c9
Jacob Vaanholt <Jacobvaanholt7@gmail.com>
2023-02-01 10:26:13 -0500

    revamped compounding calculation and DOM manipulation

--- investmentCalc.js
+++ investmentCalc.js
@@ -1,17 +1,47 @@
-//activate button to calculate
+//define the major points of contact in the DOM
 const displayResultArea = document.querySelector(".secTwo")
-const calculateBtn = document.querySelector("#investmentCalcBtn");
+const calculateBtn      = document.querySelector("#investmentCalcBtn");
+
+//code to activate the button when clicked
 calculateBtn.addEventListener("click", calculating);
 
+
+
+
+//create the elements in which the answer will be stored
+const calculateAnswerArea = document.createElement("div");
+const calculatedAnswer    = document.createElement("h3");
+calculateAnswerArea.setAttribute("id", "calculateAnswerArea");
+
+//function that manipulates the DOM and adds the answer to the file.
 function calculating(){
-    const calculateAnswerArea = document.createElement("div");
-    let principal = document.getElementById("usersPrincipalInvestment").value;
-    let holdDuration = document.getElementById("userHoldDuration").value;
-    let rateOfReturn = document.getElementById("userAvgRateOfReturn").value;
-    calculateAnswerArea.style["background-color"] = "red";
-    calculateAnswerArea.textContent = `principal = ${principal} holdDuration = ${holdDuration}
-    rateOfReturn = ${rateOfReturn}`;
+    
+    let principal               = document.getElementById("usersPrincipalInvestment").value;
+    let holdDuration            = document.getElementById("userHoldDuration").value;
+    let rateOfReturn            = document.getElementById("userAvgRateOfReturn").value;
+    let additionalContributions = document.getElementById("userAdditionalContribution").value;
+    
+    let currentSum = moneyMade(principal, holdDuration, rateOfReturn, additionalContributions);
 
+    calculatedAnswer.textContent = `Your current balance is: ${currentSum} out of this compound interest made you ${currentSum - principal}`;
 
+    calculateAnswerArea.appendChild(calculatedAnswer);
     displayResultArea.appendChild(calculateAnswerArea);
+    
+}
+
+//function that runs the compound interest calculation 
+function moneyMade(principal, additionalContributions, rateOfReturn, holdDuration){
+
+    let newAmount = 0;
+
+    let P = principal;
+    let R = rateOfReturn/100;
+    let N = 1;
+    let PMT = additionalContributions;
+    let T = holdDuration;
+    newAmount =  (P * (Math.pow((R/N+1), (N*T)))) + PMT * ((Math.pow((R/N +1), (N*T))) - 1)/(R/N);
+
+    newAmount = Math.round(newAmount*100)/100
+    return newAmount;
 }
