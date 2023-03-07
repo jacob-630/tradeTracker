481a709d2db8792f6b9ab74975dd911ffd49d433
Jacob Vaanholt <Jacobvaanholt7@gmail.com>
2023-03-06 23:30:03 -0500

    simplified and condensed code

---
+++ script.js
@@ -0,0 +1,15 @@
+//define the major points of contact in the DOM
+const signUpBtn         = document.querySelector("#signUpBtn");
+const signUpBtn2         = document.querySelector("#signUpBtn2");
+
+//code to activate the button when clicked
+signUpBtn.addEventListener("click", signUp);
+signUpBtn2.addEventListener("click", signUp);
+
+
+
+
+//Function for signing up to TradeTracker
+function signUp(){
+    window.alert("unfortunatley this feature has not been created yet");
+}
